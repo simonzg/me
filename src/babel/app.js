62d2aaa8f1ac33536d07d6660ece3059fa6b1c61
xinzhang-jessy <simonzg@gmail.com>
2017-07-22 18:12:24 -0700

    prepared portfolio page

--- src/babel/app.js
+++ src/babel/app.js
@@ -3,6 +3,20 @@ window.jQuery = window.$ = $;
 global.Tether = require('tether');
 require('bootstrap');
 
+/*=================================================
+=            Image Download Protection            =
+=================================================*/
+$('img').mousedown(function (e) {
+  if(e.button == 2) { // right click
+    return false; // do nothing!
+  }
+}
+/*=====  End of Image Download Protection  ======*/
+
+
+/*===============================
+=            Overlay            =
+===============================*/
 $('#overlay-open-btn').click(()=>{
   $('.overlay').addClass('fadein');
 })
@@ -10,3 +24,5 @@ $('#overlay-open-btn').click(()=>{
 $('#overlay-close-btn').click(()=>{
   $('.overlay').removeClass('fadein');
 })
+/*=====  End of Overlay  ======*/
+
